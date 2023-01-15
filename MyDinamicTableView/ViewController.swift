00491775e7b11ae609a5cc732b03aefa1ea633b3
Nuri Abidin <nuri.abidin@gmail.com>
2023-01-15 19:16:39 +0700

    Remove .DS_Store

--- MyDinamicTableView/ViewController.swift
+++ MyDinamicTableView/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var AcademyTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Prepare data to the TableView
+        AcademyTableView.dataSource = self
+        
+        
+        AcademyTableView.register(
+            UINib(nibName: "AcademyTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "AcademyCell"
+        )
+        
     }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyAcademyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+            withIdentifier: "AcademyCell",
+            for: indexPath
+        ) as? AcademyTableViewCell {
+            let academy = dummyAcademyData[indexPath.row]
+            cell.academyLabel.text = academy.name
+            cell.academyImageView.image = academy.image
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
+}
